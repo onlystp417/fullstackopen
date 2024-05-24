fb0721c43316837cfe512c40e057179bb5e561a1
Jin <onlystp417@gmail.com>
2024-05-24 12:54:41 -0700

    [Refac] Part1.3-1.5 1.3 Course Information step 3 1.4 Course Information step 4 1.5 Course Information step 5

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,11 +1,11 @@
 import Part from './Part'
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={parts[0].name} exercises={parts[0].exercise} />
+      <Part part={parts[1].name} exercises={parts[1].exercise} />
+      <Part part={parts[2].name} exercises={parts[2].exercise} />
     </>
   )
 }
