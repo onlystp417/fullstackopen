75db34d410db197b4aec73713efce82a9385ec5d
Jin <onlystp417@gmail.com>
2024-05-29 18:26:48 -0700

    [Build] Part1.6 - unicafe step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToComment = (comment) => () => {
+    switch(comment) {
+      case 'good':
+        setGood(good + 1)
+        break
+      case 'neutral':
+        setNeutral(neutral + 1)
+        break
+      case 'bad':
+        setBad(bad + 1)
+        break
+      default:
+        return
+    }
+  }
+
+  return (
+    <>
+      <h3>give feedback</h3>
+      <button onClick={setToComment('good')}>good</button>
+      <button onClick={setToComment('neutral')}>neutral</button>
+      <button onClick={setToComment('bad')}>bad</button>
+      <h3>statistics</h3>
+      <p>good { good }</p>
+      <p>neutral { neutral }</p>
+      <p>bad { bad }</p>
+    </>
+  )
+}
