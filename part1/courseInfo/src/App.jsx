9b4e9e9eb61af216b1e8ce3ac64bb421b291a5db
Jin <onlystp417@gmail.com>
2024-05-29 17:09:25 -0700

    Isolate every practice with a folder in part1

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,6 +1,7 @@
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+import Header from './components/Header.jsx'
+import Content from './components/Content.jsx'
+import Total from './components/Total.jsx'
+
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
