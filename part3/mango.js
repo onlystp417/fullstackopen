c85aff0f9c1871b64db35236a40b9c6eb9161465
Jin <onlystp417@gmail.com>
2025-06-08 14:16:12 +0800

    [feat] 3.12 Phonebook_bknd: Add command-line adding and to phonebook entries

--- part3/mango.js
+++ part3/mango.js
@@ -1,38 +1,34 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+console.log(process.argv)
 
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2] || process.env.MONGODB_PASSWORD
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = "mongodb+srv://onlystp417:LeH7enYeE6PH0W7Z@cluster0.1uslvx3.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0";
+const url = `mongodb+srv://onlystp417:${password}@cluster0.1uslvx3.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Phone = mongoose.model('phone', phoneSchema)
 
-// const note = new Note({
-//   content: 'JavaScript is interesting',
-//   important: false,
-// })
+const phone = new Phone({ name, number })
 
-// note.save().then(result => {
-//   console.log(result)
-//   mongoose.connection.close()
-// })
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+phone.save().then(result => {
+  console.log(`added ${result.name} number ${result.number} to phonebook`)
   mongoose.connection.close()
 })
+
