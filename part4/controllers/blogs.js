dde46b30396d97e63424b7640ac9e49a7ab87506
Jin <onlystp417@gmail.com>
2025-07-10 21:35:04 +0800

    [Refactor] Blogs: Remove try-catch from blog routes, rely on Express 5 error handling middleware

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,18 +2,13 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response, next) => {
-  try {
-    const blogs = await Blog.find({})
-    response.status(200).json(blogs)
-  } catch (exception) {
-    next(eception)
-  }
+  const blogs = await Blog.find({})
+  response.status(200).json(blogs)
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(result => response.status(200).json(result))
-    .catch(error => next(error))
+blogRouter.get('/:id', async (request, response, next) => {
+  const result = await Blog.findById(request.params.id)
+  response.status(200).json(result)
 })
 
 blogRouter.post('/', async (request, response, next) => {
@@ -31,50 +26,34 @@ blogRouter.post('/', async (request, response, next) => {
 
   const blog = new Blog(newBlog)
 
-  try {
-    const result = await blog.save()
-    response.status(201).json(result)
-  } catch(exception) {
-    next(exception)
-  }
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 blogRouter.put('/:id', async (request, response, next) => {
   const updatedBlog = request.body
-  try {
-    const result = await Blog.findByIdAndUpdate(
-      request.params.id,
-      updatedBlog,
-      { new: true, runValidators: true, overwrite: true }
-    )
-    response.status(200).json(result)
-  } catch(exception) {
-    next(exception)
-  }
+  const result = await Blog.findByIdAndUpdate(
+    request.params.id,
+    updatedBlog,
+    { new: true, runValidators: true, overwrite: true }
+  )
+  response.status(200).json(result)
 })
 
 blogRouter.patch('/:id', async (request, response, next) => {
   const updatedBlog = { ...request.body }
 
-  try {
-    const result = await Blog.findByIdAndUpdate(
-      request.params.id,
-      updatedBlog,
-      { new: true, runValidators: true }
-    )
-    response.status(200).json(result)
-  } catch(exception) {
-    next(exception)
-  }
+  const result = await Blog.findByIdAndUpdate(
+    request.params.id,
+    updatedBlog,
+    { new: true, runValidators: true }
+  )
+  response.status(200).json(result)
 })
 
 blogRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-  } catch(exception) {
-    next(exception)
-  }
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
