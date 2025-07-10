bed81726ccca0b51521b987e831529b2e1c74aad
Jin <onlystp417@gmail.com>
2025-07-10 20:16:33 +0800

    [Test] Blogs: Optimize test of hide console in test env and organize log format with describe

--- part4/test/blogs_api.test.js
+++ part4/test/blogs_api.test.js
@@ -10,80 +10,86 @@ const api = supertest(app)
 describe.only('Test Blogs API', () => {
   testHelper.dataInitialize()
 
-  test('Blogs are returned as JSON', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('GET - /api/blogs', () => {
+    test('Blogs are returned as JSON', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+  
+    test('All blogs are returned', async () => {
+      const res = await api.get('/api/blogs')
+  
+      assert.strictEqual(res.body.length, testHelper.initialBlogs.length)
+    })
+  
+    test('Blog identity field is "id", not "_id"', async () => {
+      const res = await api.get('/api/blogs')
+      for(let blog of res.body) {
+        assert.ok(blog.hasOwnProperty('id'))
+        assert.ok(!blog.hasOwnProperty('_id'))
+      }
+    })
   })
 
-  test('All blogs are returned', async () => {
-    const res = await api.get('/api/blogs')
-
-    assert.strictEqual(res.body.length, testHelper.initialBlogs.length)
+  describe('POST - /api/blogs', () => {
+    test('A valid blogs content can be added', async () => {
+      const newBlog = {
+        title: 'Backend learning road map',
+        author: 'Zack Vincene',
+        url: 'www.mediem.com/Backend_learning_road_map',
+        likes: 3936
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const blogsAtEnd = await testHelper.blogsInDb()
+  
+      assert.strictEqual(blogsAtEnd.length, testHelper.initialBlogs.length + 1)
+      
+      const titles = blogsAtEnd.map(blog => blog.title)
+      assert(titles.includes('Backend learning road map'))
+    })
+  
+    test('Initial "likes" as value 0 while it is a missing field', async () => {
+      const newBlogMissingLikes = {
+        title: 'Backend learning road map',
+        author: 'Zack Vincene',
+        url: 'www.mediem.com/Backend_learning_road_map'
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlogMissingLikes)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const blogsAtEnd = await testHelper.blogsInDb()
+      
+      const blogJustAdded = blogsAtEnd[blogsAtEnd.length - 1]
+      assert(blogJustAdded.hasOwnProperty('likes'))
+      assert.strictEqual(blogJustAdded.likes, 0)
+    })
+  
+    test('Bad request 400 while "title" or "url" properties are missing', async () => {
+      const newBlogMissingFields = {
+        author: 'Zack Vincene',
+        likes: 134
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlogMissingFields)
+        .expect(400)
+    })
   })
 
-  test('Blog identity field is "id", not "_id"', async () => {
-    const res = await api.get('/api/blogs')
-    for(let blog of res.body) {
-      assert.ok(blog.hasOwnProperty('id'))
-      assert.ok(!blog.hasOwnProperty('_id'))
-    }
-  })
 
-  test('A valid blogs content can be added', async () => {
-    const newBlog = {
-      title: 'Backend learning road map',
-      author: 'Zack Vincene',
-      url: 'www.mediem.com/Backend_learning_road_map',
-      likes: 3936
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await testHelper.blogsInDb()
-
-    assert.strictEqual(blogsAtEnd.length, testHelper.initialBlogs.length + 1)
-    
-    const titles = blogsAtEnd.map(blog => blog.title)
-    assert(titles.includes('Backend learning road map'))
-  })
-
-  test.only('Initial "likes" as value 0 while it is a missing field', async () => {
-    const newBlogMissingLikes = {
-      title: 'Backend learning road map',
-      author: 'Zack Vincene',
-      url: 'www.mediem.com/Backend_learning_road_map'
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlogMissingLikes)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await testHelper.blogsInDb()
-    
-    const blogJustAdded = blogsAtEnd[blogsAtEnd.length - 1]
-    assert(blogJustAdded.hasOwnProperty('likes'))
-    assert.strictEqual(blogJustAdded.likes, 0)
-  })
-
-    test.only('Bad request 400 while "title" or "url" properties are missing', async () => {
-    const newBlogMissingFields = {
-      author: 'Zack Vincene',
-      likes: 134
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlogMissingFields)
-      .expect(400)
-  })
 })
 
 after(async () => await mongoose.connection.close())
