928acc948b8f70e9e8f7ea0134ee3a8d4e394420
Jin <onlystp417@gmail.com>
2025-07-12 17:56:22 +0800

    [Feat] 4.17 Blogs: GET-/api/users, use populate to get user list with blog datas

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,6 +3,11 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const error = require('../utils/error')
 
+userRouter.get('/', async (req, res) => {
+  const users = await User.find({}).populate('blogs')
+  res.status(200).json(users)
+})
+
 userRouter.post('/', async (req, res, next) => {
   const { password, userName, name } = req.body
 
