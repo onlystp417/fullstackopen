81122336b4054a3ec47bb98b60e90df0488db4b0
Jin <onlystp417@gmail.com>
2025-07-24 17:24:09 +0800

    [Test] 5.17 Blogs E2E: Show login form by default

--- part4/index.js
+++ part4/index.js
@@ -3,5 +3,6 @@ const { PORT } = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.listen(PORT, () => {
+  console.log('PORT', PORT)
   logger.info(`Server running on port ${PORT}`)
 })
---
+++ playwright_blogs_testing/tests/example.spec.js
@@ -0,0 +1,31 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+const { describe, beforeEach } = test
+
+describe('when login', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+  });
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+
+  // test('has title', async ({ page }) => {
+  //   await page.goto('http://localhost:5173/');
+  
+  //   // Expect a title "to contain" a substring.
+  //   await expect(page).toHaveTitle(/Playwright/);
+  // });
+  
+  // test('get started link', async ({ page }) => {
+  //   await page.goto('https://playwright.dev/');
+  
+  //   // Click the get started link.
+  //   await page.getByRole('link', { name: 'Get started' }).click();
+  
+  //   // Expects page to have a heading with the name of Installation.
+  //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // });
+
+})
