042d49b482a937deeb5237ac8c96f5a8307b3175
Jin <onlystp417@gmail.com>
2025-07-30 18:36:20 +0800

    [Feat] 6.5 Anecdotes: Ordered by the number of votes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdotes, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
   const [anecdoteName, setAnecdoteName] = useState('')
 
