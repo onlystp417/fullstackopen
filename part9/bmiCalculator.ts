ed0d7c05442a1cb11501fe1b819b5981cf4e55a1
Jin <onlystp417@gmail.com>
2025-08-26 16:48:29 +0800

    [Feat] 9.1 bmiCalculator

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,24 @@
+function bmiCalculator (height: number, weight: number): string {
+  const heightInMeter = height / 100
+  const bmi = weight / (heightInMeter * heightInMeter)
+
+  if (bmi < 16.0)
+    return 'Underweight (Severe thinness)'
+  else if (bmi <= 17.0)
+    return 'Underweight (Moderate thinness)'
+  else if (bmi <= 18.5)
+    return 'Underweight (Mild thinness)'
+  else if (bmi <= 25)
+    return 'Normal range'
+  else if (bmi <= 30)
+    return 'Overweight (Pre-obese)'
+  else if (bmi <= 35)
+    return 'Obese (Class I)'
+  else if (bmi <= 40)
+    return 'Obese (Class II)'
+  else
+    return 'Obese (Class III)'
+}
+
+const category = bmiCalculator(159, 54)
+console.log(category)
